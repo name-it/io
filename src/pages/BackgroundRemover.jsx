890050337d075d18326a5a11b21064a424b0f726
Nikhil Sarwara <tymnec@gmail.com>
2023-11-29 19:08:40 +0530

    python running sample

--- src/pages/BackgroundRemover.jsx
+++ src/pages/BackgroundRemover.jsx
@@ -3,14 +3,30 @@ import Layout from "../components/Layout";
 import { WallpaperRounded } from "@mui/icons-material";
 import Button from "../components/Button";
 import ReactImageUploading from "react-images-uploading";
+import { useState, useEffect } from "react";
 
 function BackgroundRemover() {
   const [selectedImage, setSelectedImage] = React.useState(null);
+  const [output, setOutput] = useState("(loading...)");
+  const runScript = () => {
+    window.pyodide.loadPackage([]).then(() => {
+      const output = window.pyodide.runPython("5+7");
+      setOutput(output);
+      console.log(output);
+    });
+  };
+
+  useEffect(() => {
+    window.languagePluginLoader;
+  });
 
   return (
     <Layout>
       <div>
         <h1 className="text-3xl font-bold">Background Remover</h1>
+        {/* Running Python */}
+        <p>5 + 7 = {output}</p>
+        <Button onClick={runScript}>Run Python</Button>
 
         <ReactImageUploading
           value={selectedImage}
