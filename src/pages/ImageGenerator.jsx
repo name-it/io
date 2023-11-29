9dd2a955f8a263410a3b9db2e675d0cbac9cdfe8
Nikhil Sarwara <tymnec@gmail.com>
2023-11-29 23:49:16 +0530

    change model

--- src/pages/ImageGenerator.jsx
+++ src/pages/ImageGenerator.jsx
@@ -5,9 +5,11 @@ import Button from "../components/Button";
 
 function ImageGenerator() {
   const [image, setImage] = useState(null);
+  const [text, setText] = useState("");
+
   async function query(data) {
     const response = await fetch(
-      "https://api-inference.huggingface.co/models/stabilityai/stable-diffusion-xl-base-1.0",
+      "https://api-inference.huggingface.co/models/openskyml/dalle-3-xl",
       {
         headers: {
           Authorization: `Bearer ${
@@ -24,10 +26,18 @@ function ImageGenerator() {
   return (
     <Layout>
       <h1 className="text-3xl font-bold">Image Generator</h1>
-      <Button onClick={() => query({ inputs: "Astronaut riding a horse" })}>
-        Create Image
-      </Button>
 
+      {/* Input */}
+      <div className="flex gap-3 place-items-center my-3">
+        <p className="text-stone-700 font-bold">Enter your text: </p>
+        <input
+          type="text"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+          className="border border-stone-800 p-1 text-stone-700"
+        />
+      </div>
+      <Button onClick={() => query({ inputs: text })}>Create Image</Button>
       {/* Display Image if available */}
       {image && <img src={image} alt="Created image" className="w-full" />}
     </Layout>
