a60434b952d763cbdcfa259f21f3810cd03892ba
Nikhil Sarwara <tymnec@gmail.com>
2023-11-29 13:12:51 +0530

    add home and about page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import { createBrowserRouter } from "react-router-dom";
 import { RouterProvider } from "react-router-dom";
+import Home from "./pages/Home";
 
 // Create router
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello World</div>,
+    element: <Home />,
+  },
+  {
+    path: "/about",
+    element: <div>About</div>,
   },
 ]);
 
