a0e79cf8cf774961731e22ec25d18ea288fdf1aa
Nikhil Sarwara <tymnec@gmail.com>
2023-11-30 14:39:03 +0530

    simple input field added

--- src/pages/ImageResizer.jsx
+++ src/pages/ImageResizer.jsx
@@ -1,19 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import Layout from "../components/Layout";
 import Button from "../components/Button";
-import { useState } from "react";
 import ImageUploader from "../components/ImageUploader";
 
 function ImageResizer() {
-  const [selectedImage, setSelectedImage] = React.useState(null);
+  const [selectedImage, setSelectedImage] = useState(null);
+  const [resizedImage, setResizedImage] = useState(null);
+  const [width, setWidth] = useState(
+    selectedImage ? selectedImage[0].width : ""
+  );
+  const [height, setHeight] = useState(
+    selectedImage ? selectedImage[0].height : ""
+  );
 
-  //   Function to Handle Download Image
   const handleDownloadImage = () => {
-    console.log(selectedImage[0]);
     const link = document.createElement("a");
-    link.href = selectedImage[0].data_url;
-    link.download = "image.png";
+    link.href = resizedImage;
+    link.download = "resized_image.png";
     link.click();
+  };
+
+  const handleResizeImage = () => {
+    if (selectedImage && selectedImage[0]) {
+      const img = new Image();
+      img.src = selectedImage[0].data_url;
+
+      img.onload = () => {
+        const canvas = document.createElement("canvas");
+        const ctx = canvas.getContext("2d");
+
+        canvas.width = parseInt(width, 10) || img.width;
+        canvas.height = parseInt(height, 10) || img.height;
+
+        ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+
+        const resizedDataURL = canvas.toDataURL("image/png");
+        setResizedImage(resizedDataURL);
+      };
+    }
   };
 
   return (
@@ -28,15 +52,38 @@ function ImageResizer() {
         setSelectedImage={setSelectedImage}
       />
 
-      {/* If image is available then Download Button should be visible */}
-      {selectedImage && (
-        <Button
-          variant="primary"
-          iconBefore="download"
-          onClick={handleDownloadImage}
-        >
-          Download
+      {/* Input fields for width and height */}
+      <div className="mt-4 flex space-x-4">
+        <input
+          type="number"
+          placeholder="Width"
+          value={width}
+          onChange={(e) => setWidth(e.target.value)}
+        />
+        <input
+          type="number"
+          placeholder="Height"
+          value={height}
+          onChange={(e) => setHeight(e.target.value)}
+        />
+        <Button variant="primary" onClick={handleResizeImage}>
+          Resize
         </Button>
+      </div>
+
+      {/* Display Resized Image */}
+      {resizedImage && (
+        <div className="mt-4">
+          <img src={resizedImage} alt="Resized" style={{ maxWidth: "100%" }} />
+          <Button
+            className="mt-2"
+            variant="primary"
+            iconBefore="download"
+            onClick={handleDownloadImage}
+          >
+            Download Resized
+          </Button>
+        </div>
       )}
     </Layout>
   );
