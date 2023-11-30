5220420c7482e1698ba94ca46d0b985823fb02c9
Nikhil Sarwara <tymnec@gmail.com>
2023-11-30 13:53:32 +0530

    used image uploader in image resizer

--- src/pages/ImageResizer.jsx
+++ src/pages/ImageResizer.jsx
@@ -1,12 +1,22 @@
 import React from "react";
 import Layout from "../components/Layout";
+import Button from "../components/Button";
+import { useState } from "react";
+import ImageUploader from "../components/ImageUploader";
 
 function ImageResizer() {
+  const [selectedImage, setSelectedImage] = React.useState(null);
   return (
     <Layout>
       <div>
         <h1 className="text-3xl font-bold">Image Resizer</h1>
       </div>
+
+      {/* Image Uploader */}
+      <ImageUploader
+        selectedImage={selectedImage}
+        setSelectedImage={setSelectedImage}
+      />
     </Layout>
   );
 }
